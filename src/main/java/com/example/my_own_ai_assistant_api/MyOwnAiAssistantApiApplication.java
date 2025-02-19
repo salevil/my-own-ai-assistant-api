f8365b2aa9cc84ed617baacf5cce52afb7178cc6
LEVILLAIN Steve Adrien Marie <sam.levillain@soprasteria.com>
2025-02-19 14:37:22 +0100

    First Commit starting the app

---
+++ src/main/java/com/example/my_own_ai_assistant_api/MyOwnAiAssistantApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_own_ai_assistant_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnAiAssistantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOwnAiAssistantApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/my_own_ai_assistant_api/MyOwnAiAssistantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_own_ai_assistant_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOwnAiAssistantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
